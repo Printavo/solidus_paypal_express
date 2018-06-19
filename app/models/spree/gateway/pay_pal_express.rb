304c9bf20396ac2ae9b57bc27084bc01a2efeb47
Piotr Biegaj <PeteTheSadPanda@users.noreply.github.com>
2018-06-19 15:45:55 -0500

    Formatting

--- app/models/spree/gateway/pay_pal_express.rb
+++ app/models/spree/gateway/pay_pal_express.rb
@@ -36,46 +36,46 @@ module Spree
       'paypal'
     end
 
-		def purchase(amount, express_checkout, gateway_options={})
-			pp_details_request = provider.build_get_express_checkout_details(
-			{
-				Token: express_checkout.token
-			}
-			)
-			pp_details_response = provider.get_express_checkout_details(pp_details_request)
+    def purchase(amount, express_checkout, gateway_options={})
+      pp_details_request = provider.build_get_express_checkout_details(
+        {
+          Token: express_checkout.token
+        }
+      )
+      pp_details_response = provider.get_express_checkout_details(pp_details_request)
 
-			pp_request = provider.build_do_express_checkout_payment(
-			{
-				DoExpressCheckoutPaymentRequestDetails: {
-					PaymentAction: "Sale",
-					Token: express_checkout.token,
-					PayerID: express_checkout.payer_id,
-					PaymentDetails: pp_details_response.get_express_checkout_details_response_details.PaymentDetails
-				}
-			}
-			)
+      pp_request = provider.build_do_express_checkout_payment(
+        {
+          DoExpressCheckoutPaymentRequestDetails: {
+            PaymentAction: "Sale",
+            Token: express_checkout.token,
+            PayerID: express_checkout.payer_id,
+            PaymentDetails: pp_details_response.get_express_checkout_details_response_details.PaymentDetails
+          }
+        }
+      )
 
-			pp_response = provider.do_express_checkout_payment(pp_request)
-			if pp_response.success?
-				# We need to store the transaction id for the future.
-				# This is mainly so we can use it later on to refund the payment if the user wishes.
-				transaction_id = pp_response.do_express_checkout_payment_response_details.payment_info.first.transaction_id
-				express_checkout.update_column(:transaction_id, transaction_id)
-				# This is rather hackish, required for payment/processing handle_response code.
-				Class.new do
-					def success?; true; end
-					def authorization; nil; end
-				end.new
-			else
-				class << pp_response
-					def to_s
-						errors.map(&:long_message).join(" ")
-					end
-				end
-				pp_response
-			end
-		end
-		
+      pp_response = provider.do_express_checkout_payment(pp_request)
+      if pp_response.success?
+        # We need to store the transaction id for the future.
+        # This is mainly so we can use it later on to refund the payment if the user wishes.
+        transaction_id = pp_response.do_express_checkout_payment_response_details.payment_info.first.transaction_id
+        express_checkout.update_column(:transaction_id, transaction_id)
+        # This is rather hackish, required for payment/processing handle_response code.
+        Class.new do
+          def success?; true; end
+          def authorization; nil; end
+        end.new
+      else
+        class << pp_response
+          def to_s
+            errors.map(&:long_message).join(" ")
+          end
+        end
+        pp_response
+      end
+    end
+
     # amount :: float
     # express_checkout :: Spree::PaypalExpressCheckout
     # gateway_options :: hash
@@ -97,36 +97,36 @@ module Spree
       do_capture(amount_cents, authorization, currency)
     end
 
-		def refund(payment, amount)
-			refund_type = payment.amount == amount.to_f ? "Full" : "Partial"
-			refund_transaction = provider.build_refund_transaction(
-				{ TransactionID: payment.source.transaction_id,
-					RefundType: refund_type,
-					Amount: {
-						currencyID: payment.currency,
-						value: amount },
-					RefundSource: "any" })
-			refund_transaction_response = provider.refund_transaction(refund_transaction)
-			if refund_transaction_response.success?
-				payment.source.update_attributes(
-					{ refunded_at: Time.now,
-						refund_transaction_id: refund_transaction_response.RefundTransactionID,
-						state: "refunded",
-						refund_type: refund_type
-					})
+    def refund(payment, amount)
+      refund_type = payment.amount == amount.to_f ? "Full" : "Partial"
+      refund_transaction = provider.build_refund_transaction(
+        { TransactionID: payment.source.transaction_id,
+          RefundType: refund_type,
+          Amount: {
+            currencyID: payment.currency,
+            value: amount },
+          RefundSource: "any" })
+      refund_transaction_response = provider.refund_transaction(refund_transaction)
+      if refund_transaction_response.success?
+        payment.source.update_attributes(
+          { refunded_at: Time.now,
+            refund_transaction_id: refund_transaction_response.RefundTransactionID,
+            state: "refunded",
+            refund_type: refund_type
+          })
 
-				payment.class.create!(
-					order: payment.order,
-					source: payment,
-					payment_method: payment.payment_method,
-					amount: amount.to_f.abs * -1,
-					response_code: refund_transaction_response.RefundTransactionID,
-					state: 'completed'
-				)
-			end
-			refund_transaction_response
-		end
-		 
+        payment.class.create!(
+          order: payment.order,
+          source: payment,
+          payment_method: payment.payment_method,
+          amount: amount.to_f.abs * -1,
+          response_code: refund_transaction_response.RefundTransactionID,
+          state: 'completed'
+        )
+      end
+      refund_transaction_response
+    end
+
     def credit(credit_cents, transaction_id, originator:, **_options)
       payment = originator.payment
       amount = credit_cents / 100.0
@@ -149,7 +149,7 @@ module Spree
             refund_transaction_id: refund_transaction_response.RefundTransactionID,
             state: "refunded",
             refund_type: refund_type
-        })
+          })
       end
 
       build_response(
@@ -172,7 +172,7 @@ module Spree
         "https://www.#{server_domain}paypal.com/cgi-bin/webscr?" +
           "cmd=_express-checkout&force_sa=true&"
       end +
-      URI.encode_www_form(params)
+        URI.encode_www_form(params)
     end
 
     def do_authorize(token, payer_id)
@@ -180,7 +180,7 @@ module Spree
         self.
         provider.
         do_express_checkout_payment(
-          checkout_payment_params(token, payer_id))
+        checkout_payment_params(token, payer_id))
 
       build_response(response, authorization_transaction_id(response))
     end
@@ -188,11 +188,11 @@ module Spree
     def do_capture(amount_cents, authorization, currency)
       response = provider.
         do_capture(
-          provider.build_do_capture(
-            amount: amount_cents / 100.0,
-            authorization_id: authorization,
-            complete_type: "Complete",
-            currencycode: options[:currency]))
+        provider.build_do_capture(
+          amount: amount_cents / 100.0,
+          authorization_id: authorization,
+          complete_type: "Complete",
+          currencycode: options[:currency]))
 
       build_response(response, capture_transaction_id(response))
     end
@@ -224,7 +224,7 @@ module Spree
       self.
         provider.
         get_express_checkout_details(
-          checkout_details_params(token)).
+        checkout_details_params(token)).
         get_express_checkout_details_response_details.
         PaymentDetails
     end
@@ -233,10 +233,10 @@ module Spree
       self.
         provider.
         build_do_express_checkout_payment(
-          build_checkout_payment_params(
-            token,
-            payer_id,
-            payment_details(token)))
+        build_checkout_payment_params(
+          token,
+          payer_id,
+          payment_details(token)))
     end
 
     def checkout_details_params(token)
