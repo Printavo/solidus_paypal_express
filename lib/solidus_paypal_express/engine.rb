ee479190ae0ead91035f5f996fbac72d048d1f4b
Piotr Biegaj <PeteTheSadPanda@users.noreply.github.com>
2021-01-16 11:10:21 -0600

    Update loading of decorators to the more modern approach of using SolidusSupport::EngineExtension

--- lib/solidus_paypal_express/engine.rb
+++ lib/solidus_paypal_express/engine.rb
@@ -1,5 +1,7 @@
 module SolidusPayPalExpress
   class Engine < Rails::Engine
+    include SolidusSupport::EngineExtensions
+
     require 'spree/core'
     isolate_namespace Spree
     engine_name 'solidus_paypal_express'
@@ -11,16 +13,8 @@ module SolidusPayPalExpress
       g.test_framework :rspec
     end
 
-    def self.activate
-      Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_decorator*.rb')) do |c|
-        require_dependency(c)
-      end
-    end
-
-    config.to_prepare &method(:activate).to_proc
-
     initializer "spree.paypal_express.payment_methods", after: "spree.register.payment_methods" do |app|
-      app.config.spree.payment_methods << Spree::Gateway::PayPalExpress
+      app.config.spree.payment_methods << "Spree::Gateway::PayPalExpress"
     end
   end
 end
